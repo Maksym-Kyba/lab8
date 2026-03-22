848911a41560413ae05f5c22c7efa9f605f3f2b8
Maksym_Kyba <maksymkyba09@gmail.com>
2026-03-22 19:05:53 +0200

    Add basic logic, basic test passed

---
+++ sim_parse_linked_list/parsing.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def linked_list_from_string(list_repr: str) -> Node | None:
+    values = list_repr.split(' -> ')
+    for ind, val in enumerate(values):
+        try:
+            integer = int(val)
+            values[ind] = integer
+        except ValueError:
+            pass
+    latest_node = Node(values[-1], None)
+    values = values[:-1][::-1]
+    for val in values:
+        latest_node = Node(val, latest_node)
+
+    return latest_node
