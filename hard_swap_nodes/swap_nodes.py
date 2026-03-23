adb60afd37e5e30b6389a4c72bbda2d6937e0734
Maksym_Kyba <maksymkyba09@gmail.com>
2026-03-23 09:23:23 +0200

    Check if linked list is empty or with only one node

--- hard_swap_nodes/swap_nodes.py
+++ hard_swap_nodes/swap_nodes.py
@@ -5,17 +5,16 @@ class Node:
 def swap_pairs(head):
     current = head
     previous_pair = None
-    pair_skipper = head
+    if head is not None and head.next is not None:
+        head = head.next
     while current is not None and current.next is not None:
         next_node = current.next
-        current_node = current
         following_pair = next_node.next
-        next_node.next = current_node
-        current_node.next = following_pair
-
+        next_node.next = current
+        current.next = following_pair
 
         if previous_pair is not None:
             previous_pair.next = next_node
-        previous_pair = current_node
+        previous_pair = current
         current = following_pair
     return head
