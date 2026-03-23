3990d303d11c8afc22efaf0611661e4e0efd9d32
Maksym_Kyba <maksymkyba09@gmail.com>
2026-03-23 09:51:46 +0200

    Deleted unneeded variables and changed logic

--- hard_alternating_split/alt_split.py
+++ hard_alternating_split/alt_split.py
@@ -12,25 +12,18 @@ def alternating_split(head):
     current = head
     change_list = 0
     first_list = Node(None)
-    previous_first_list = current.data
     second_list = Node(None)
-    previous_second_list = current.next.data
     current = current.next.next
     while current is not None and current.next is not None:
         change_list += 1
         if change_list % 2 == 1:
-            first_list.data = previous_first_list
-            first_list.next = current.next.data
-            previous_first_list = first_list
+            new_node = Node(current.data)
+            first_list.next = new_node
+            first_list = new_node
         if change_list % 2 == 0:
-            second_list.data = previous_second_list
-            second_list.next = current.next.data
-            previous_second_list = second_list
+            new_node = Node(current.data)
+            second_list.next = new_node
+            second_list = new_node
         current = current.next
-
-    first_list.data = previous_first_list
-    first_list.next = None
-    second_list.data = previous_second_list
-    second_list.next = None
 
     return Context(first_list, second_list)
